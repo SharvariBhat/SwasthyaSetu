ac8abd6961060447064dbbd4649163515704ba05
SharvariBhat <sharvarisiri@gmail.com>
2026-05-01 14:34:07 +0530

    supabase db connection done

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,28 +1,26 @@
-const mysql = require("mysql2");
+const { Pool } = require("pg");
 require("dotenv").config();  // enables us to use process.env which helps us in accessing .env variables  
 
-const db = mysql.createPool({  // using createPool instead of createConnection to allow for multiple connections and better performance
+const db = new Pool({  // using Pool instead of createConnection to allow for multiple connections and better performance
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT,
 
-  waitForConnections: true,  // allows the pool to create a queue of connection requests when all connections are in use
-  connectionLimit: 10,  // limits the number of connections in the pool 
-  queueLimit: 0  // no limit on the number of queued connection requests
+  ssl: {
+    rejectUnauthorized: false  // this is necessary for connecting to some cloud databases that require SSL, but it should be used with caution in production environments
+  },
+
+  max: 10, // connection limit
 });
 
-// Test connection immediately
-db.getConnection((err, connection) => {
-  if (err) {
+db.connect()  // testing the connection to the database
+  .then(() => {
+    console.log("Database connected successfully");
+  })
+  .catch((err) => {
     console.error("Database connection failed:", err.message);
-    return;
-  }
-
-  console.log("Database connected successfully");
-
-  connection.release();  // releases the connection back to the pool so that it can be resued by other requests
-});
+  });
 
 module.exports = db;
